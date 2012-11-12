8aa442290be95d1a79da3cd5432414d93d7a2127
antojoseph <antojoseph007@gmail.com>
2012-11-12 16:49:47 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -3,33 +3,10 @@
 from myclass import *
 from hasher import *
 
-
 hashme= hashguru()
 x=dbhandler("adadada")
 x.mkdb()
 
-
-def show():	
-	print ("")
-	print ("Printing Contents of the DB !")    
-	print ("")
-	result = x.show()
-	for row in result:
-		print(row[0],row[1],row[2])
-	print ("")
-	print ("")
-
-
-
-
-def checkhash():
-	result=x.show()
-	for row in result:
-		sum_now= hashme.hashit(row[1])
-		if (sum_now == row[2]):
-			print("file is not changed ")
-		else:
-			print("file has changed ")
 
 # Checking if argument was provided
 
@@ -38,16 +15,12 @@ if (len(sys.argv)<=1):
     hashme.print_banner()
     print ("Error ! No filename specified ")
 elif(sys.argv[1]=="-check"):
-		checkhash()
+		x.checkhash()
 elif(sys.argv[1]=="-show"):
-		show()
+		x.show()
 else:
-    try:
-    	hashme.print_banner()
-    	f =  open(sys.argv[1],'rb')
-    	f.close()        
-    	hashsum = hashme.hashit(sys.argv[1])
-    	x.insert(sys.argv[1],hashsum)
-    except:
-    	print("File Doesn't Exist or You don't have Sufficent Privilages !")
+    hashme.print_banner()     
+    hashsum = hashme.hashit(sys.argv[1])
+    x.insert(sys.argv[1],hashsum)
+
 
--- myclass.py
+++ myclass.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3.2
 import sqlite3
+from hasher import *
+hashme= hashguru()
 class dbhandler:
 	def __init__(self,db_name):
 		self.db =db_name
@@ -28,13 +30,31 @@ class dbhandler:
 	def insert(self,filename,filehash):
 		self.filename=filename
 		self.filehash=filehash
-		db.execute('insert into test (fname, fhash) values (?,?)',(filename,filehash))
-		db.commit()
+		try:
+			db.execute('insert into test (fname, fhash) values (?,?)',(filename,filehash))
+			db.commit()
+		except:
+			print("File Doesn't Exist or You don't have Sufficent Privilages !")
+
 
 	def show(self):
 		result = db.execute('select * from test')
+		print ("")
+		print ("Printing Contents of the DB !")    
+		print ("")
 		for row in result:
-			yield (row['primary'],row['fname'],row['fhash'])
+			print(row[0],row[1],row[2])
+		print ("")
+		print ("")
+
+	def checkhash(self):
+		result = db.execute('select * from test')
+		for row in result:
+			sum_now= hashme.hashit(row[1])
+			if (sum_now == row[2]):
+				print("file is not changed ")
+			else:
+				print("file has changed ")
 
 
 
